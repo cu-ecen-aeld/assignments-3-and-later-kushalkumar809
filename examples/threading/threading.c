e788758b457b6e7668974173a9bcde3251590708
root <root@Ubuntu.myguest.virtualbox.org>
2025-02-17 23:18:19 +0530

    working assignemt 4 part 1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -14,6 +14,25 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    
+    usleep((thread_func_args -> wait_to_obtain_ms) * 1000);
+    
+    int rc = pthread_mutex_lock((thread_func_args -> mutex));
+    if (rc != 0) 
+    {
+#define ERROR_LOG(msg,...) printf("threading ERROR: " msg "\n" , ##__VA_ARGS__)
+    }
+    
+    usleep((thread_func_args -> wait_to_release_ms) * 1000);
+    
+    rc = pthread_mutex_unlock((thread_func_args -> mutex));
+    
+    if (rc != 0) 
+    {
+#define ERROR_LOG(msg,...) printf("threading ERROR: " msg "\n" , ##__VA_ARGS__)
+    }
+    
     return thread_param;
 }
 
@@ -28,6 +47,25 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+
+    struct thread_data *data = (struct thread_data *)malloc(sizeof(struct thread_data));
+    data->mutex = mutex;
+    data->wait_to_obtain_ms = wait_to_obtain_ms;
+    data->wait_to_release_ms = wait_to_release_ms;
+    data->thread_complete_success = false;
+    
+    int rc = pthread_create(thread,NULL,threadfunc,(void*)data);
+    if (rc != 0) {
+	#define ERROR_LOG(msg,...) printf("threading ERROR: " msg "\n" , ##__VA_ARGS__)
+        free(data); // Free allocated memory on error
+        data->thread_complete_success=false;
+        return data->thread_complete_success;
+    }
+    
+    else
+    {
+    data->thread_complete_success=true;
+    return data->thread_complete_success;
+    }
 }
 
