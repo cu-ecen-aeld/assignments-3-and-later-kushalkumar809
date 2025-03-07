3c5339ce4c2d28a9cc75d897497efecaf7b9f834
root <root@Ubuntu.myguest.virtualbox.org>
2025-03-07 10:49:16 -0700

    needs debug

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -5,28 +5,35 @@
 #include <errno.h>
 #include <syslog.h>
 #include <string.h>
+#include <signal.h>
+#include <sys/stat.h> 
+#include <netdb.h>
+#include <netinet/in.h> 
+#include <unistd.h>
 
-#define port="9000";
+#define port "9000"
+#define buff_len 500
 
-int main ()
+
+int main (int argc,char **argv)
 {
 struct addrinfo serv;
 struct addrinfo *servinfo;
 
 memset (&serv, 0, sizeof(serv));
 
-socket.ai_family = AF_inet;
-socket.ai_socktype = SOCK_STREAM;
-socket.ai_flags = 0;
-socket.protocol = 0;
+serv.ai_family = AF_INET;
+serv.ai_socktype = SOCK_STREAM;
+serv.ai_flags = 0;
+serv.ai_protocol = 0;
 
 
 
-if (getaddrinfo(NULL,port,&socket,&servinfo);!=0)
+if (getaddrinfo(NULL,port,&serv,&servinfo)!=0)
 {
-int errnum = erno;
+int errnum = errno;
 char *errmesg = strerror(errno);
-openlog(NULL,0,LOG_ERROR);
+openlog(NULL,0,LOG_ERR);
 syslog(LOG_ERR,"GETADDRINFO ERROR %d : %s",errnum,errmesg);
 closelog();
 return -1;
@@ -36,9 +43,9 @@ int socketfd = socket((*servinfo).ai_family, (*servinfo).ai_socktype, (*servinfo
 
 if (socketfd == -1)
 {
-int errnum = erno;
+int errnum = errno;
 char *errmesg = strerror(errno);
-openlog(NULL,0,LOG_ERROR);
+openlog(NULL,0,LOG_ERR);
 syslog(LOG_ERR,"SOCKET OPENING ERROR %d : %s",errnum,errmesg);
 closelog();
 freeaddrinfo(servinfo);
@@ -47,11 +54,11 @@ return -1;
 
 
 
-if (bind((*servinfo).ai_addr, (*servinfo).ai_addrlen)) !=0)
+if (bind(socketfd,(*servinfo).ai_addr, (*servinfo).ai_addrlen) !=0)
 {
-int errnum = erno;
+int errnum = errno;
 char *errmesg = strerror(errno);
-openlog(NULL,0,LOG_ERROR);
+openlog(NULL,0,LOG_ERR);
 syslog(LOG_ERR,"SOCKET IP BINDING ERROR %d : %s",errnum,errmesg);
 closelog();
 freeaddrinfo(servinfo);
@@ -62,33 +69,78 @@ openlog(NULL,0,LOG_INFO);
 syslog(LOG_INFO,"SOCKET OPEN SUCCESS");
 closelog();
 
-if (listen(socketfd,port) == -1)
+if (listen(socketfd,0) == -1)
 {
-int errnum = erno;
+int errnum = errno;
 char *errmesg = strerror(errno);
-openlog(NULL,0,LOG_ERROR);
-syslog(LOG_ERR,"Listen to port %d failed (%d) : %s",port ,errnum ,errmesg);
+openlog(NULL,0,LOG_ERR);
+syslog(LOG_ERR,"Listen to port %s failed (%d) : %s",port ,errnum ,errmesg);
 closelog();
 freeaddrinfo(servinfo);
 return -1;
 }
 
 openlog(NULL,0,LOG_INFO);
-syslog(LOG_INFO,"LISTENING TO PORT %d"port);
-acceptedfd = accept(socketfd, (*servinfo).ai_adrr, (*servinfo).addr_len); 
-
-if(acceptedfd == -1)
-{
-syslog(LOG_INFO,"No Client Connected");
+syslog(LOG_INFO,"LISTENING TO PORT %s",port);
 closelog();
-freeaddrinfo(servinfo);
-return -1;
+
+
+
+for (;;)
+{
+struct sockaddr_storage client_addr;
+socklen_t client_addr_len = sizeof(client_addr);
+int acceptedfd = accept(socketfd, (struct sockaddr *)&client_addr, &client_addr_len); 
+	if(acceptedfd == -1)
+	{
+	openlog(NULL,0,LOG_INFO);
+	syslog(LOG_INFO,"Connection accept failed");
+	closelog();
+	}
+	
+	else 
+	{
+	        openlog(NULL,0,LOG_INFO);
+		//syslog(LOG_INFO,"Accepted Connection from %s",client_addr);
+		closelog();
+		
+		///printf("\nAccepted Connection from %s\n",client_addr);
+
+		for (;;)
+		{
+		char buff[buff_len];
+		recv(acceptedfd,buff,sizeof(buff),0);
+		printf("1: %s",buff);
+		
+		FILE *fptr = fopen("/var/tmp/aesdsocketdata","a");
+		fputs(buff,fptr);
+		fclose(fptr);
+		FILE *rptr = fopen("/var/tmp/aesdsocketdata","r");
+		
+		int line[10];
+		size_t read = fread(line,sizeof(line),sizeof(*line),rptr);
+		send(acceptedfd,line, sizeof(line) ,0);
+		
+		char line[256];
+		 while (fgets(line, sizeof(line), fptr) != NULL) 
+		{
+        		printf("2 :%s", line); // Print the line
+        		send(acceptedfd,line, sizeof(line) ,0);
+   		}
+   		
+		
+		fclose(rptr);
+		
+		}
+		
+	}
+
+
+
 }
 
 
 
-
 closelog();
-
 freeaddrinfo(servinfo);
 }
