d9edb41a95f72a40f1a73d037e6fc85b03714fdc
root <root@Ubuntu.myguest.virtualbox.org>
2025-03-09 13:55:39 -0600

    working git add .

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -10,13 +10,45 @@
 #include <netdb.h>
 #include <netinet/in.h> 
 #include <unistd.h>
+#include <fcntl.h>
 
 #define port "9000"
-#define buff_len 500
+#define BUFFER_SIZE 500
+
+volatile sig_atomic_t keep_running = 1;
+int socketfd;
+int acceptedfd;
+const char *filename = "/var/tmp/aesdsocketdata";
+
+void signal_handler(int signo) {
+    if (signo == SIGINT || signo == SIGTERM) {
+        printf("Caught signal, exiting\n");
+        if (acceptedfd > 0)
+    	{
+            close(acceptedfd);
+    	}
+    	if (socketfd > 0)
+    	{
+            close(socketfd);
+    	}
+        keep_running = 0;
+        remove(filename);
+    }
+}
 
 
 int main (int argc,char **argv)
 {
+
+signal(SIGINT, signal_handler);
+signal(SIGTERM, signal_handler);
+
+int daemon_mode = 0;
+    if (argc > 1 && strcmp(argv[1], "-d") == 0) {
+        daemon_mode = 1;
+    }
+    
+
 struct addrinfo serv;
 struct addrinfo *servinfo;
 
@@ -39,7 +71,7 @@ closelog();
 return -1;
 }
 
-int socketfd = socket((*servinfo).ai_family, (*servinfo).ai_socktype, (*servinfo).ai_protocol);
+socketfd = socket((*servinfo).ai_family, (*servinfo).ai_socktype, (*servinfo).ai_protocol);
 
 if (socketfd == -1)
 {
@@ -85,12 +117,42 @@ syslog(LOG_INFO,"LISTENING TO PORT %s",port);
 closelog();
 
 
-
-for (;;)
-{
 struct sockaddr_storage client_addr;
 socklen_t client_addr_len = sizeof(client_addr);
-int acceptedfd = accept(socketfd, (struct sockaddr *)&client_addr, &client_addr_len); 
+
+
+while (keep_running)
+{
+
+ if (daemon_mode) {
+        pid_t pid = fork();
+        if (pid < 0) {
+            perror("fork failed");
+            exit(1);
+        }
+        if (pid > 0) {
+            exit(0);
+        }
+
+        setsid();
+        chdir("/");
+     
+        close(STDIN_FILENO);
+        close(STDOUT_FILENO);
+        close(STDERR_FILENO);
+
+        int fd = open("/dev/null", O_RDWR);
+        if (fd != -1) {
+            dup2(fd, STDIN_FILENO);
+            dup2(fd, STDOUT_FILENO);
+            dup2(fd, STDERR_FILENO);
+            close(fd);
+        }
+    }
+
+
+
+acceptedfd = accept(socketfd, (struct sockaddr *)&client_addr, &client_addr_len); 
 	if(acceptedfd == -1)
 	{
 	openlog(NULL,0,LOG_INFO);
@@ -100,47 +162,67 @@ int acceptedfd = accept(socketfd, (struct sockaddr *)&client_addr, &client_addr_
 	
 	else 
 	{
-	        openlog(NULL,0,LOG_INFO);
-		//syslog(LOG_INFO,"Accepted Connection from %s",client_addr);
-		closelog();
-		
-		///printf("\nAccepted Connection from %s\n",client_addr);
+		int s;
+		char host[NI_MAXHOST], service[NI_MAXSERV];
+		s=getnameinfo((struct sockaddr *)&client_addr,client_addr_len, host, NI_MAXHOST,service, NI_MAXSERV, NI_NUMERICSERV);
+		if(s==0)
+   		   {
+   		   	openlog(NULL,0,LOG_INFO);
+			syslog(LOG_INFO,"Accepted connection from %s", service);
+			closelog();
+      		   }
+		ssize_t bytes_read;
+		char buffer[BUFFER_SIZE];
+		while ((bytes_read = read(acceptedfd, buffer, sizeof(buffer))) > 0) 
+		{
+			char *newline = memchr(buffer,'\n',bytes_read);
+			if (newline)
+			{
+				
+                	size_t line_length = newline - buffer + 1;
+                	int fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
+                	if (fd == -1) {
+                    		perror("Error opening file");
+                    	continue;
+			}
+			
+			write(fd, buffer, line_length);
+                	close(fd);
+                	
+                	int fd_read = open(filename, O_RDONLY);
+                	
+                	char buffer2[BUFFER_SIZE];
+                	while ((bytes_read = read(fd_read, buffer2, sizeof(buffer2))) > 0) 
+                	{
+                   		send(acceptedfd, buffer2, bytes_read, 0);
+               		 }
+               		 
+               		  close(fd_read);
+	
+			}
+			
+			
+			else
+           	 	{	
+              	  		int fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
+               		 	if (fd == -1) {
+                    			perror("Error opening file");
+                    			continue;
+                			      }		
+               	 		write(fd, buffer, bytes_read);
+                 		close(fd);
+            		} 
+			
 
-		for (;;)
-		{
-		char buff[buff_len];
-		recv(acceptedfd,buff,sizeof(buff),0);
-		printf("1: %s",buff);
-		
-		FILE *fptr = fopen("/var/tmp/aesdsocketdata","a");
-		fputs(buff,fptr);
-		fclose(fptr);
-		FILE *rptr = fopen("/var/tmp/aesdsocketdata","r");
-		
-		int line[10];
-		size_t read = fread(line,sizeof(line),sizeof(*line),rptr);
-		send(acceptedfd,line, sizeof(line) ,0);
-		
-		char line[256];
-		 while (fgets(line, sizeof(line), fptr) != NULL) 
-		{
-        		printf("2 :%s", line); // Print the line
-        		send(acceptedfd,line, sizeof(line) ,0);
-   		}
-   		
-		
-		fclose(rptr);
-		
 		}
-		
-	}
 
-
+	}		
 
 }
+	shutdown(socketfd, SHUT_RDWR);
+	close(socketfd);
+	freeaddrinfo(servinfo);
+	closelog();
+	return 0;
 
-
-
-closelog();
-freeaddrinfo(servinfo);
 }
